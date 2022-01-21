c4801d1ef5af09902b196a94f53d38e10a147ab4
AbellasJuan <juanvictoraa@gmail.com>
2022-01-20 23:46:02 -0300

    post/get inicializing with an example

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ import cors from 'cors';
 const server = express();
 server.use(cors());
 
-server.get('/' , (req, res)=> {
-    res.send('test');
+server.use(express.json());
+
+const exemplos = [];
+
+server.get('/postagem' , (req, res)=> {
+    res.send(exemplos);
+});
+
+server.post('/postagem', (req, res) => {
+    const exemplo = req.body;
+    exemplos.push(exemplo);
+    res.send(exemplo);
 });
 
 server.listen(5000);
