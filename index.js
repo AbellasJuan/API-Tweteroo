55b1251a11cb78b6f9ff5257f7396baa18ba2455
AbellasJuan <juanvictoraa@gmail.com>
2022-01-22 19:53:47 -0300

    tweets and sign up done

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ server.use(cors());
 
 server.use(express.json());
 
+const users = [];
 const tweets = [];
 
 server.get('/tweets' , (req, res)=> {
-    res.send(tweets);
+    const primeiros10Tweets = tweets.slice(0-10);
+    
+    res.send(primeiros10Tweets);
 });
 
 server.post('/sign-up', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    console.log(user.avatar);
     res.send('OK');
 });
 
 server.post('/tweets', (req, res) => {
     const tweet = req.body;
+    
+    const exemplo = {...tweet, testePropriedade: 'teste'}
+    console.log(exemplo, users);
+   
     tweets.push(tweet);
     res.send('OK');
 })
