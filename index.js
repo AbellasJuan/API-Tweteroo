7a322213ee6baaf7cea21ba4fc9726b87c3d6dcf
AbellasJuan <juanvictoraa@gmail.com>
2022-01-24 11:37:22 -0300

    sign up validation

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ server.use(express.json());
 const users = [];
 const tweets = [];
 
+server.post('/sign-up', (req, res) => {
+    const user = req.body;
+
+    console.log(user)
+    console.log(user.avatar.length)
+    
+    if(user.username.length === 0){
+        return res.sendStatus(400);
+    }
+
+    if(user.avatar.length === 0){
+        return res.sendStatus(400);
+    }
+    
+    users.push(user);
+    res.send('OK');
+});
+
 server.get('/tweets' , (req, res)=> {
     const primeiros10Tweets = tweets.slice(0-10);
     const primeiros10ComOrdemCorreta = primeiros10Tweets.reverse();
     res.send(primeiros10ComOrdemCorreta);
-});
-
-server.post('/sign-up', (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.send('OK');
 });
 
 server.post('/tweets', (req, res) => {
