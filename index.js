c2d685e2d7c168c2089c0d868e6ca8a0955969ab
abellasjuan <juanvictoraa@gmail.com>
2022-01-27 17:50:46 -0300

    bonus done

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import cors from 'cors';
 
 const server = express();
 server.use(cors());
-
-server.use(express.json());
+server.use(json());
 
 const users = [];
 const tweets = [];
@@ -25,30 +24,45 @@ server.post('/sign-up', (req, res) => {
 });
 
 server.get('/tweets' , (req, res)=> {
-    const primeiros10Tweets = tweets.slice(0-10);
-    const primeiros10ComOrdemCorreta = primeiros10Tweets.reverse();
-    res.send(primeiros10ComOrdemCorreta);
+    if(!req.query.page || parseInt(req.query.page) < 1) {
+        return res.status(400).send("Informe uma página válida!");
+    }
+    
+    const page = parseInt(req.query.page);
+    const limit = 10;
+    const startIndex = (page - 1 ) * limit;
+    const endIndex = page * limit;
+
+    const primeiros10Tweets = [... tweets].reverse().slice(startIndex, endIndex);
+    res.send(primeiros10Tweets);
 });
 
-server.post('/tweets', (req, res) => {
-    const tweet = req.body;
-    const currentUser = users.find(user => user.username === tweet.username);
-    const tweetWithAvatar = {...tweet, avatar: currentUser.avatar};
-    
+server.post('/tweets', (req, res) => {    
+    const currentUser = users.find(user => user.username === req.headers.user);
+    const tweetWithAvatar = { tweet: req.body.tweet , username: req.headers.user , avatar: currentUser.avatar};
+
     if(tweetWithAvatar.username.length === 0 || tweetWithAvatar.username.trim().length === 0 ){
-        return res.status(400).send('Todos os campos são obrigatórios!')
+        return res.status(400).send('Todos os campos são obrigatórios!');
     }
 
     if(tweetWithAvatar.avatar.length === 0 || tweetWithAvatar.avatar.trim().length === 0){
-        return res.status(400).send('Todos os campos são obrigatórios!')
+        return res.status(400).send('Todos os campos são obrigatórios!');
     }
 
     if(tweetWithAvatar.tweet.length === 0 || tweetWithAvatar.tweet.trim().length === 0 ){
-        return res.status(400).send('Todos os campos são obrigatórios!')
+        return res.status(400).send('Todos os campos são obrigatórios!');
     }
 
     tweets.push(tweetWithAvatar);
     res.status(201).send(tweetWithAvatar);
+})
+
+server.get('/tweets/:username', (req, res) => {
+    const tweetsFiltradosPorUsuario = tweets.filter(tweet => tweet.username === req.params.username);
+
+    const tweetsFiltradosPorUsuarioComOrdemCorreta = tweetsFiltradosPorUsuario.reverse();
+
+    res.send(tweetsFiltradosPorUsuarioComOrdemCorreta);
 })
 
 server.listen(5000);
